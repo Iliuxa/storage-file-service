36bbfec0c808d96c0d569365740a4014916246f2
Олег <oleg.oj17@yandex.ru>
2025-03-09 21:40:08 +0300

    feat(): DownloadFile

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -1,6 +1,7 @@
 package storagegrpc
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"errors"
@@ -8,6 +9,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"io"
+	"os"
 	"storage-file-service/internal/domain"
 	"storage-file-service/internal/usecase"
 	proto "storage-file-service/proto/gen"
@@ -15,20 +17,32 @@ import (
 
 type serverApi struct {
 	proto.UnimplementedFileServiceServer
-	storage usecase.StorageUsecase
+	storage           usecase.StorageUsecase
+	downloadSemaphore chan struct{}
+	listSemaphore     chan struct{}
 }
 
 func Register(gRPCServer *grpc.Server, storage usecase.StorageUsecase) {
-	proto.RegisterFileServiceServer(gRPCServer, &serverApi{storage: storage})
+	proto.RegisterFileServiceServer(
+		gRPCServer,
+		&serverApi{
+			storage:           storage,
+			downloadSemaphore: make(chan struct{}, 10),
+			listSemaphore:     make(chan struct{}, 100),
+		},
+	)
 }
 
 func (s *serverApi) GetFileList(
 	ctx context.Context,
 	in *proto.FileListRequest,
 ) (*proto.FileListResponse, error) {
+	s.listSemaphore <- struct{}{}
+	defer func() { <-s.listSemaphore }()
+
 	files, err := s.storage.GetFileList(ctx)
 	if err != nil {
-		if errors.Is(err, domain.ErrUsersGettingError) {
+		if errors.Is(err, domain.ErrFileGettingError) {
 			return nil, status.Error(codes.Internal, "Failed to getting file")
 		}
 		return nil, status.Error(codes.Internal, "Failed to getting file")
@@ -39,6 +53,9 @@ func (s *serverApi) GetFileList(
 func (s *serverApi) UploadFile(
 	stream grpc.ClientStreamingServer[proto.UploadRequest, proto.UploadResponse],
 ) error {
+	s.downloadSemaphore <- struct{}{}
+	defer func() { <-s.downloadSemaphore }()
+
 	req, err := stream.Recv()
 	if err != nil {
 		panic(333)
@@ -49,12 +66,12 @@ func (s *serverApi) UploadFile(
 	imageData := bytes.Buffer{}
 
 	for {
-		err := contextError(stream.Context())
+		err = contextError(stream.Context())
 		if err != nil {
 			return err
 		}
 
-		req, err := stream.Recv()
+		req, err = stream.Recv()
 		if err == io.EOF {
 			break
 		}
@@ -84,8 +101,51 @@ func (s *serverApi) UploadFile(
 
 func (s *serverApi) DownloadFile(
 	in *proto.FileRequest,
-	str grpc.ServerStreamingServer[proto.FileResponse],
+	stream grpc.ServerStreamingServer[proto.FileResponse],
 ) error {
+	s.downloadSemaphore <- struct{}{}
+	defer func() { <-s.downloadSemaphore }()
+
+	fileInfo, filePath, err := s.storage.DownloadFile(stream.Context(), uint(in.GetFileId()))
+	if err != nil {
+		return status.Error(codes.Internal, "Failed to download file")
+	}
+
+	err = stream.Send(&proto.FileResponse{Data: fileInfo})
+	if err != nil {
+		return status.Error(codes.Internal, "Failed to download file")
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return status.Error(codes.Internal, "Failed to download file")
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1024)
+
+	for {
+		err = contextError(stream.Context())
+		n, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return status.Errorf(codes.Unknown, "cannot read chunck to buffer: %v", err)
+		}
+
+		res := &proto.FileResponse{
+			Data: &proto.FileResponse_ChunkData{
+				ChunkData: buffer[:n],
+			},
+		}
+
+		err = stream.Send(res)
+		if err != nil {
+			return status.Errorf(codes.Unknown, "cannot send chunk to the client: %v", err)
+		}
+	}
 
 	return nil
 }
